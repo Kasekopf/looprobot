f38556b7f8b57b147183dd52e4d6a2ea9022f41b
Kasekopf <jeffreydudek@gmail.com>
2024-05-19 14:38:03 -0700

    Add some spring shoes drops to junk

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -893,7 +893,7 @@ function autosellJunk(): void {
   if (have($item`MayDay™ supply package`)) use($item`MayDay™ supply package`);
 
   // Sell junk items
-  const junk = $items`hamethyst, baconstone, meat stack, dense meat stack, facsimile dictionary, space blanket, 1\,970 carat gold, black snake skin, demon skin, hellion cube, adder bladder, weremoose spit, Knob Goblin firecracker, wussiness potion, diamond-studded cane, Knob Goblin tongs, Knob Goblin scimitar, eggbeater, red-hot sausage fork, Knob Goblin pants, awful poetry journal, black pixel, pile of dusty animal bones, 1952 Mickey Mantle card, liquid ice, fat stacks of cash`;
+  const junk = $items`hamethyst, baconstone, meat stack, dense meat stack, facsimile dictionary, space blanket, 1\,970 carat gold, black snake skin, demon skin, hellion cube, adder bladder, weremoose spit, Knob Goblin firecracker, wussiness potion, diamond-studded cane, Knob Goblin tongs, Knob Goblin scimitar, eggbeater, red-hot sausage fork, Knob Goblin pants, awful poetry journal, black pixel, pile of dusty animal bones, 1952 Mickey Mantle card, liquid ice, fat stacks of cash, juniper berries, peach, pear, plum`;
   for (const item of junk) {
     if (have(item)) autosell(item, itemAmount(item));
   }
