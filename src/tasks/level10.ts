6b9a4aa702d18a3fa2dbf1c1a70b2070e8afd3ba
Ignose <ihpares@gmail.com>
2024-10-16 11:37:00 -0400

    Don't use the force in airship with bat wings

--- src/tasks/level10.ts
+++ src/tasks/level10.ts
@@ -103,7 +103,7 @@ export const GiantQuest: Quest = {
         if (have($effect`Temporary Amnesia`)) cliExecute("uneffect Temporary Amnesia");
       },
       orbtargets: () => {
-        if (have($item`Fourth of May Cosplay Saber`)) {
+        if (have($item`Fourth of May Cosplay Saber`) && !have($item`bat wings`)) {
           if (have($item`Mohawk wig`)) return $monsters`Quiet Healer`;
           else return $monsters`Quiet Healer, Burly Sidekick`;
         } else {
@@ -128,7 +128,7 @@ export const GiantQuest: Quest = {
           if (have($item`Mohawk wig`)) return new Macro();
           if (have($skill`Emotionally Chipped`) && get("_feelEnvyUsed") < 3)
             return Macro.skill($skill`Feel Envy`).step(killMacro());
-          if (haveEquipped($item`Fourth of May Cosplay Saber`) && get("_saberForceUses") < 5)
+          if (haveEquipped($item`Fourth of May Cosplay Saber`) && get("_saberForceUses") < 5 && !have($item`bat wings`))
             return Macro.skill($skill`Use the Force`);
           return new Macro();
         }, $monster`Burly Sidekick`)
@@ -147,7 +147,7 @@ export const GiantQuest: Quest = {
       },
       orbtargets: () => [],
       outfit: () => {
-        if (get("_saberForceUses") < 5 && !have($item`Mohawk wig`)) {
+        if (get("_saberForceUses") < 5 && !have($item`Mohawk wig`) && !have($item`bat wings`)) {
           return {
             modifier: "-combat",
             weapon: $item`Fourth of May Cosplay Saber`,
@@ -168,7 +168,7 @@ export const GiantQuest: Quest = {
         if (have($item`Mohawk wig`)) return new Macro();
         if (have($skill`Emotionally Chipped`) && get("_feelEnvyUsed") < 3)
           return Macro.skill($skill`Feel Envy`).step(killMacro());
-        if (haveEquipped($item`Fourth of May Cosplay Saber`) && get("_saberForceUses") < 5)
+        if (haveEquipped($item`Fourth of May Cosplay Saber`) && get("_saberForceUses") < 5 && !have($item`bat wings`))
           return Macro.skill($skill`Use the Force`);
         return new Macro();
       }, $monster`Burly Sidekick`),
