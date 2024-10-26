2dc5f25a74bf961d36ede5317344b7e592b6bacc
Kasekopf <jeffreydudek@gmail.com>
2024-10-26 15:48:38 -0700

    Leave room for tinsel tights

--- src/tasks/level9.ts
+++ src/tasks/level9.ts
@@ -149,7 +149,7 @@ const Oil: Task[] = [
 
       const spec: OutfitSpec & { equip: Item[] } = {
         modifier: `ML ${mlNeeded} max, 0.1 item, 0.1 food drop`,
-        equip: $items`unbreakable umbrella, unwrapped knock-off retro superhero cape, June cleaver, tearaway pants`,
+        equip: $items`unbreakable umbrella, unwrapped knock-off retro superhero cape, June cleaver`,
         modes: { umbrella: "broken", retrocape: ["heck", "hold"] },
         avoid: $items`Kramco Sausage-o-Matic™`,
       };
@@ -200,7 +200,7 @@ const Oil: Task[] = [
       if (have($item`unbreakable umbrella`))
         return {
           modifier: "ML 80 max, 0.1 item, 0.1 food drop, monster level percent",
-          equip: $items`unbreakable umbrella, Everfull Dart Holster, unwrapped knock-off retro superhero cape, tearaway pants`,
+          equip: $items`unbreakable umbrella, Everfull Dart Holster, unwrapped knock-off retro superhero cape`,
           modes: { umbrella: "broken", retrocape: ["heck", "hold"] },
         };
       else
